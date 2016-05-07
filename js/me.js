9eb1ca510a04403453936738d23856c0c538c810
Michael Fix <mfix@wisc.edu>
2016-05-07 15:26:39 -0500

    Particles.js

--- js/me.js
+++ js/me.js
@@ -1,27 +1,29 @@
-document.getElementById('main').appendChild
-
-var stars = []
-for(var i = 0; i <84; i++){
-	var left = 0;
-	var topD = 0;
-	left = Math.floor((Math.random() * 90) + 5);
-	topD = Math.floor((Math.random() * 90) + 5);
-	// while (true){
-	// 	left = Math.floor((Math.random() * 94) + 2);
-	// 	topD = Math.floor((Math.random() * 90) + 5);
-	// 	if ((left < 5 || left > 35	) || (topD < 5 || topD > 90)) 
-	// 		break;
-	// }		
-	var star = document.createElement('img');
-	stars.push(star);
-	star.setAttribute('src','img/star.png');
-	star.setAttribute('class', 'star');
-	star.style.top = topD + "%";
-	star.style.left = left + "%";
-	var deg = Math.floor((Math.random() * 360));
-	star.style.transform = "rotate(" + deg + "deg)";
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+// var stars = []
+// for(var i = 0; i <84; i++){
+// 	var left = 0;
+// 	var topD = 0;
+// 	left = Math.floor((Math.random() * 90) + 5);
+// 	topD = Math.floor((Math.random() * 90) + 5);
+// 	// while (true){
+// 	// 	left = Math.floor((Math.random() * 94) + 2);
+// 	// 	topD = Math.floor((Math.random() * 90) + 5);
+// 	// 	if ((left < 5 || left > 35	) || (topD < 5 || topD > 90)) 
+// 	// 		break;
+// 	// }		
+// 	var star = document.createElement('img');
+// 	stars.push(star);
+// 	star.setAttribute('src','img/star.png');
+// 	star.setAttribute('class', 'star');
+// 	star.style.top = topD + "%";
+// 	star.style.left = left + "%";
+// 	var deg = Math.floor((Math.random() * 360));
+// 	star.style.transform = "rotate(" + deg + "deg)";
 	
-	document.getElementById('main').appendChild(star);
+// 	document.getElementById('main').appendChild(star);
 	
-}
+// }
 // $("#main").append('<img src="star.png"/>');
