e0b391d54a07366418325bb868d44cb4b4e62c07
Michael Fix <mfix@wisc.edu>
2016-08-13 14:33:14 -0700

    change Bandwagon copy

--- bin/bundle.js
+++ bin/bundle.js
@@ -32965,10 +32965,10 @@
 
 	module.exports = [{
 	  'id': 'bandwagon',
-	  'desc': 'US Patent 9321503 B2 for collapsible bike trailer. Invention for transporting higher volumes while still allowing for easy storage. This project was completed alongside Katrina L. Ruedinger during freshman year at UW Madison.',
+	  'desc': 'Invention known at the Bandwagon Bike Trailer. This collapsible bike trailer can transport higher volumes while still storing easily. This project was completed alongside Katrina L. Ruedinger during freshman year at UW Madison.',
 	  'img': 'bandwagon.png',
 	  'link': 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1=9321503.PN.&OS=PN/9321503&RS=PN/9321503',
-	  'header': 'Bandwagon Bike Trailer',
+	  'header': 'US Patent 9321503 B2',
 	  'technologies': []
 	}, {
 	  'id': 'resource_map',
