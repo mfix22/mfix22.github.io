a765c62bd10594d397e4c703e51025b9245a2f21
Mike Fix <mrfix84@gmail.com>
2017-03-26 18:05:41 -0500

    update automaton

--- assets/projects.js
+++ assets/projects.js
@@ -15,7 +15,7 @@ const {
   ATOM
 } = require('../assets/company_logos')
 
-module.exports =  {
+module.exports = {
   nature: {
     desc: 'Atom theme inspired by nature, built with Javascript and React in mind.',
     img: 'nature-syntax.png',
@@ -49,100 +49,100 @@ module.exports =  {
     value: 2
   },
   bandwagon: {
-    desc : 'Invention known at the Bandwagon Bike Trailer. This collapsible bike trailer can transport higher volumes while still storing easily. This project was completed alongside Katrina L. Ruedinger during freshman year at UW Madison.',
-    img : 'bandwagon.png',
-    link : 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1=9321503.PN.&OS=PN/9321503&RS=PN/9321503',
-    header : 'US Patent 9321503 B2',
-    technologies : [],
+    desc: 'Invention known at the Bandwagon Bike Trailer. This collapsible bike trailer can transport higher volumes while still storing easily. This project was completed alongside Katrina L. Ruedinger during freshman year at UW Madison.',
+    img: 'bandwagon.png',
+    link: 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1=9321503.PN.&OS=PN/9321503&RS=PN/9321503',
+    header: 'US Patent 9321503 B2',
+    technologies: [],
     value: 9
   },
   resource_map: {
-    desc : 'Map of Madison, WI entrepreneurship ecosystem',
-    img : 'map.png',
-    link : 'http://transcend.engineering/map',
-    header : 'Entrepreneurship Map',
-    technologies : [STRIPE, MAPBOX, JS, JQUERY, HTML, CSS],
-    moon : true,
+    desc: 'Map of Madison, WI entrepreneurship ecosystem',
+    img: 'map.png',
+    link: 'http://transcend.engineering/map',
+    header: 'Entrepreneurship Map',
+    technologies: [STRIPE, MAPBOX, JS, JQUERY, HTML, CSS],
+    moon: true,
     value: 5
   },
   librarify: {
-    desc : 'Create a Node.js library instantly from a REST API',
-    img : 'librarify.png',
-    link : 'https://www.npmjs.com/package/librarify',
-    header : 'Librarify',
-    technologies : [NODE, JS],
+    desc: 'Create a Node.js library instantly from a REST API',
+    img: 'librarify.png',
+    link: 'https://www.npmjs.com/package/librarify',
+    header: 'Librarify',
+    technologies: [NODE, JS],
     value: 9
   },
   what3words: {
-    desc : 'Node client for the what3words API',
-    img : 'what3words.png',
-    link : 'https://www.npmjs.com/package/what3words',
-    header : 'what3words',
-    technologies : [NODE, JS],
+    desc: 'Node client for the what3words API',
+    img: 'what3words.png',
+    link: 'https://www.npmjs.com/package/what3words',
+    header: 'what3words',
+    technologies: [NODE, JS],
     value: 9
   },
   squadbot: {
-    desc : 'Bot that helps you meet up with your team in real life.',
-    img : 'squadbot.png',
+    desc: 'Bot that helps you meet up with your team in real life.',
+    img: 'squadbot.png',
     link: 'http://bot.squadup.io',
-    header : 'SquadBot',
-    technologies : [SLACK, NODE, MONGO, JS],
+    header: 'SquadBot',
+    technologies: [SLACK, NODE, MONGO, JS],
     value: 8
   },
   automaton: {
-    desc : 'Advertising that drives itself, custom site.',
-    img : 'automaton.png',
-    link: 'http://automatonads.com',
-    header : 'Automaton',
-    technologies : [CSS, HTML, INKSCAPE, JQUERY, JS],
+    desc: 'Advertising that drives itself, custom site.',
+    img: 'automaton.png',
+    link: 'https://github.com/mfix22/automaton-site',
+    header: 'Automaton',
+    technologies: [CSS, HTML, INKSCAPE, JQUERY, JS],
     value: 2
   },
   transcend: {
-    desc : 'Transcend, UW Madison innovative hub',
-    img : 'transcend.png',
+    desc: 'Transcend, UW Madison innovative hub',
+    img: 'transcend.png',
     link: 'http://transcend.engineering',
-    header : 'Transcend',
-    technologies : [HTML, CSS, SASS],
+    header: 'Transcend',
+    technologies: [HTML, CSS, SASS],
     value: 5
   },
   tm: {
-    desc : 'Transcend Madison, by-students-for-students innovation competition',
-    img : 'tm.png',
+    desc: 'Transcend Madison, by-students-for-students innovation competition',
+    img: 'tm.png',
     link: 'http://transcend.engineering/competition',
-    header : 'Transcend Madison',
-    technologies : [],
+    header: 'Transcend Madison',
+    technologies: [],
     value: 5
   },
   fab: {
-    desc : 'Floating button; add it to any site as a help tool',
-    img : 'fab_square.png',
+    desc: 'Floating button; add it to any site as a help tool',
+    img: 'fab_square.png',
     link: 'https://github.com/mfix22/floating_help_button',
-    header : 'Floating Action Button',
-    technologies : [HTML, CSS, JS],
+    header: 'Floating Action Button',
+    technologies: [HTML, CSS, JS],
     value: 4
   },
   eventbot: {
-    desc : 'Event production Slack slash command tool',
-    img : 'eventbot.png',
+    desc: 'Event production Slack slash command tool',
+    img: 'eventbot.png',
     link: 'https://github.com/mfix22/event_bot',
-    header : 'EventBot',
-    technologies : [SLACK, NODE, JS],
+    header: 'EventBot',
+    technologies: [SLACK, NODE, JS],
     value: 9
   },
   rideshare: {
-    desc : 'Custom mobile friendly site for ridesharing promotion',
-    img : 'rideshare.png',
+    desc: 'Custom mobile friendly site for ridesharing promotion',
+    img: 'rideshare.png',
     link: 'https://github.com/mfix22/ride_share_promo',
-    header : 'Rideshare San Diego',
-    technologies : [HTML, CSS],
+    header: 'Rideshare San Diego',
+    technologies: [HTML, CSS],
     value: 2
   },
   buz: {
-    desc : 'Hyperlocal communication. Look up and meet people.',
-    img : 'buz_square.png',
+    desc: 'Hyperlocal communication. Look up and meet people.',
+    img: 'buz_square.png',
     link: 'https://github.com/zshires/buz',
-    header : 'Buz',
-    technologies : [ANDROID, INKSCAPE],
+    header: 'Buz',
+    technologies: [ANDROID, INKSCAPE],
     value: 9
   }
 }
