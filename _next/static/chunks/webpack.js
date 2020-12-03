d4f25d20bf0f07ef00bb2ff84249ff3e39b59e48
Mike Fix <mrfix84@gmail.com>
2020-12-02 18:24:59 -0700

    new build

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c96908d811807ddf1649";
+/******/ 	var hotCurrentHash = "6d4c5537437fc1cdfdf5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
