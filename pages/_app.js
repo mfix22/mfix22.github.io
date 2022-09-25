51934c69e086bb14bf40274840f45c169418149c
Mike Fix <mrfix84@gmail.com>
2022-09-24 17:49:24 -0700

    add nav item

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+import { Link } from '../components/Link'
 import { Icon } from '../components/Icon'
 
 import '../public/global.css'
@@ -10,13 +11,33 @@ export default function App({ Component, pageProps }) {
     children = (
       <>
         <nav>
-          <Icon href="https://twitter.com/fixitup2" name="logo-twitter" />
-          <Icon href="https://github.com/mfix22" name="logo-github" />
+          <div>
+            <Icon href="https://twitter.com/fixitup2" name="logo-twitter" />
+            <Icon href="https://github.com/mfix22" name="logo-github" />
+          </div>
+          <Link className="item" href="/observations">
+            observations
+          </Link>
         </nav>
         <main>
           <h1>{pageProps.markdoc.frontmatter.title}</h1>
           <Component {...pageProps} />
         </main>
+        <style jsx>
+          {`
+            nav {
+              display: flex;
+              flex-direction: column;
+              align-items: flex-end;
+            }
+
+            nav :global(.item) {
+              margin-right: 10px;
+              margin-bottom: 8px;
+              font-size: 10px;
+            }
+          `}
+        </style>
       </>
     )
   } else {
